97f766c77b259995d0d71e2cf447ed022b0ae2d4
Christophe Riccio <christophe.riccio@gmail.com>
2010-05-19 19:55:33 +0100

    Add all OpenGL 1.1 declarations

--- glo/core/core100.hpp
+++ glo/core/core100.hpp
@@ -10,9 +10,72 @@
 #ifndef glo_core100_included
 #define glo_core100_included
 
+// Base OpenGL types
+typedef unsigned int GLenum;
+typedef unsigned char GLboolean;
+typedef unsigned int GLbitfield;
+typedef signed char GLbyte;
+typedef short GLshort;
+typedef int GLint;
+typedef int GLsizei;
+typedef unsigned char GLubyte;
+typedef unsigned short GLushort;
+typedef unsigned int GLuint;
+typedef float GLfloat;
+typedef float GLclampf;
+typedef double GLdouble;
+typedef double GLclampd;
+typedef void GLvoid;
+
 // OpenGL 1.0 functions
-void glCullFace(GLenum mode);
-GLenum glGetError();
+void glCullFace (GLenum mode);
+void glFrontFace (GLenum mode);
+void glHint (GLenum target, GLenum mode);
+void glLineWidth (GLfloat width);
+void glPointSize (GLfloat size);
+void glPolygonMode (GLenum face, GLenum mode);
+void glScissor (GLint x, GLint y, GLsizei width, GLsizei height);
+void glTexParameterf (GLenum target, GLenum pname, GLfloat param);
+void glTexParameterfv (GLenum target, GLenum pname, const GLfloat * params);
+void glTexParameteri (GLenum target, GLenum pname, GLint param);
+void glTexParameteriv (GLenum target, GLenum pname, const GLint * params);
+void glTexImage1D (GLenum target, GLint level, GLint internalformat, GLsizei width, GLint border, GLenum format, GLenum type, const GLvoid *pixels);
+void glTexImage2D (GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid *pixels);
+void glDrawBuffer (GLenum mode);
+void glClear (GLbitfield mask);
+void glClearColor (GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha);
+void glClearStencil (GLint s);
+void glClearDepth (GLclampd depth);
+void glStencilMask (GLuint mask);
+void glColorMask (GLboolean red, GLboolean green, GLboolean blue, GLboolean alpha);
+void glDepthMask (GLboolean flag);
+void glDisable (GLenum cap);
+void glEnable (GLenum cap);
+void glFinish (void);
+void glFlush (void);
+void glBlendFunc (GLenum sfactor, GLenum dfactor);
+void glLogicOp (GLenum opcode);
+void glStencilFunc (GLenum func, GLint ref, GLuint mask);
+void glStencilOp (GLenum fail, GLenum zfail, GLenum zpass);
+void glDepthFunc (GLenum func);
+void glPixelStoref (GLenum pname, GLfloat param);
+void glPixelStorei (GLenum pname, GLint param);
+void glReadBuffer (GLenum mode);
+void glReadPixels (GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *pixels);
+void glGetBooleanv (GLenum pname, GLboolean *params);
+void glGetDoublev (GLenum pname, GLdouble *params);
+GLenum glGetError (void);
+void glGetFloatv (GLenum pname, GLfloat *params);
+void glGetIntegerv (GLenum pname, GLint *params);
+const GLubyte * glGetString (GLenum name);
+void glGetTexImage (GLenum target, GLint level, GLenum format, GLenum type, GLvoid *pixels);
+void glGetTexParameterfv (GLenum target, GLenum pname, GLfloat *params);
+void glGetTexParameteriv (GLenum target, GLenum pname, GLint *params);
+void glGetTexLevelParameterfv (GLenum target, GLint level, GLenum pname, GLfloat *params);
+void glGetTexLevelParameteriv (GLenum target, GLint level, GLenum pname, GLint *params);
+GLboolean glIsEnabled (GLenum cap);
+void glDepthRange (GLclampd near, GLclampd far);
+void glViewport (GLint x, GLint y, GLsizei width, GLsizei height);
 
 #include "core100.inl"
 
