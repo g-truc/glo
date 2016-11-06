acb1f7cc8461f89c76b29615aa16277cdf87c884
Christophe Riccio <christopheri@unity3d.com>
2016-11-06 20:05:15 +0100

    Consolidate external files

--- gl/gl5.h
+++ gl/gl5.h
@@ -5,7 +5,45 @@
 extern "C" {
 #endif
 
+#include <cstdint>
 
+typedef void* GLdevice;
+typedef void* GLtexture;
+typedef void* GLbuffer;
+typedef void* GLpass;
+typedef void* GLpipeline;
+
+enum GLtextureTarget
+{
+
+};
+
+enum GLbufferTarget
+{
+
+};
+
+GLdevice glCreateDevice();
+void glReleaseDevice(GLdevice Device);
+void glFlush(GLdevice Device);
+void glDraw(GLdevice Device, std::uint32_t indexCount, std::uint32_t instanceCount, std::uint32_t firstIndex, std::uint32_t vertexOffset, std::uint32_t firstInstance);
+
+GLtexture glCreateTexture();
+void glReleaseTexture(GLtexture Texture);
+void glBindTexture(GLdevice Device, GLtextureTarget textureTarget, GLtexture Texture);
+
+GLbuffer glCreateBuffer();
+void glReleaseBuffer(GLbuffer Buffer);
+void glBindBuffer(GLdevice Device, GLbufferTarget BufferTarget, GLbuffer Buffer);
+
+GLpass glCreatePass();
+void glReleasePass(GLpass Pass);
+void glBeginPass(GLdevice Device, GLpass Pass);
+void glEndPass(GLdevice Device, GLpass Pass);
+
+GLpipeline glCreatePipeline();
+void glReleasePipeline(GLpipeline Pipeline);
+void glBindPipeline(GLdevice Device, GLpipeline Pipeline);
 
 #ifdef __cplusplus
 }
